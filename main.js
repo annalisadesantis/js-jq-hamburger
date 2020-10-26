d33279354987c7ca2f3bbf37edcfc45827eccf4a
Annalisa <annalisadesantis770@gmail.com>
2020-10-26 15:40:44 +0100

    aggiunta slidetoggle in chiusura

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Al click sull'icona dell'hamburger menu, deve essere visualizzato il menu mobile e al click sull'icona "X" il menu mobile deve scomparire.
 // In altre parole, dovere scrivere solo il codice js necessario a far funzionare l'hamburger menu.
 
+
 // Hamburger-menu appare al click
 $(".fa-bars").click(function(){
     $(".hamburger-menu").slideToggle("slow");
@@ -8,5 +9,5 @@ $(".fa-bars").click(function(){
 
 // Hamburger-menu scompare al click
 $(".close").click(function(){
-    $(".hamburger-menu").hide();
+    $(".hamburger-menu").slideToggle();
 });
